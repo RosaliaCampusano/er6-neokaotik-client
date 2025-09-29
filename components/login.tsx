8a8c16251e4b787c76a4b4dc5aa6eb74a6148fe3
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-09-29 10:47:20 +0200

    feat(NK-7): implement the Google Sign In logic and convert the Google token to Firebase token

--- components/login.tsx
+++ components/login.tsx
@@ -1,14 +1,49 @@
-import React from "react";
-import { Container, Image, Button, Text } from "../styled-components/loginStyled";
+import React, { useEffect } from 'react';
+import {
+  Container,
+  Image,
+  Button,
+  Text,
+} from '../styled-components/loginStyled';
+import { GoogleAuth } from 'react-native-google-auth';
+import { webClientId, androidClientId } from '../credentials';
+import auth from '@react-native-firebase/auth';
 
 const Login = () => {
-    return (
-        <Container>
-                <Image source={require("../assets/login.png")} />
-            <Button >
-                <Text>Sign In</Text>
-                </Button>
-        </Container>
-    )
-}
+  useEffect(() => {
+    configuereGoogleAuth();
+  }, []);
+
+  return (
+    <Container>
+      <Image source={require('../assets/login-screen.jpg')} />
+      <Button onPress={SignIn}>
+        <Text>Sign In with Google</Text>
+      </Button>
+    </Container>
+  );
+};
+
+const configuereGoogleAuth = async () => {
+  await GoogleAuth.configure({
+    webClientId: webClientId,
+    androidClientId: androidClientId,
+  });
+};
+
+const SignIn = async () => {
+  const response = await GoogleAuth.signIn();
+
+  if (response.type === 'success') {
+    const { idToken } = response.data;
+
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+    const userCredential = await auth().signInWithCredential(googleCredential);
+
+    const firebaseUser = await userCredential.user.getIdToken();
+
+    // --- Insert the spinner here ---
+  }
+};
+
 export default Login;
--- styled-components/loginStyled.ts
+++ styled-components/loginStyled.ts
@@ -1,39 +1,37 @@
-import styled from "styled-components/native";
+import styled from 'styled-components/native';
 
 const Container = styled.View`
-    width: 100%;
-    height: 100%;
-    justify-content: center;
-    align-items: center;
-`
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Image = styled.Image`
-    width: 100%;
-    height: 100%;
-    transform: scale(1.1);
-    filter: brightness(2);
-`
+  width: 100%;
+  height: 100%;
+  transform: scale(1.1);
+  filter: brightness(2);
+`;
 
 const Button = styled.TouchableOpacity`
-    width: 40%;
-    height: 70px;
-    position: absolute;
-    top: 300px;
-    background-color: transparent;
-    border: 5px solid;
-    border-radius: 10px;
-    margin-left: 1px;
+  width: 45%;
+  height: 60px;
+  position: absolute;
+  top: 300px;
+  background-color: transparent;
+  border: 5px solid white;
+  border-radius: 10px;
+  margin-left: 1px;
 `;
 
 const Text = styled.Text`
-    font-size: 20px;
-    font-weight: bold;
-    color: #000000ff;
-    position: absolute;
-    left: 35px;
-    top: 15px;
-`
+  font-size: 14px;
+  font-weight: bold;
+  color: #ffffffff;
+  position: absolute;
+  left: 15px;
+  top: 15px;
+`;
 
-export {
-    Container, Image, Button, Text
-}
+export { Container, Image, Button, Text };
