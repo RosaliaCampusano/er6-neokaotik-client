a9662bbb1396254ecd360e387cb1e9987173f807
Rosalia Campusano <rosalia.campusano@ikasle.aeg.eus>
2025-10-01 20:41:05 +0200

    feat(NK-36): implement the spinner and the response for the request to display the error message or change the screen

--- components/login.tsx
+++ components/login.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Container,
   Image,
   Button,
   Text,
+  SpinnerWrapper,
 } from '../styled-components/loginStyled';
 import { GoogleAuth } from 'react-native-google-auth';
 import auth from '@react-native-firebase/auth';
-import { sendTokenRequest } from '../api - request/tokenResquest';
+import { sendTokenRequest } from '../request/tokenResquest';
 import { AppState } from '../helpers/constants';
+import { CircleFade } from 'react-native-animated-spinkit';
 
 interface LoginProps {
   setActualState: (state: number) => void;
   setErrorMessage: (message: string) => void;
+  setUser: any;
 }
 
-const Login = ({ setActualState, setErrorMessage }) => {
+const Login = ({ setActualState, setErrorMessage, setUser }: LoginProps) => {
+  const [loading, setLoading] = useState(false);
+
   const SignIn = async () => {
     const response = await GoogleAuth.signIn();
 
     if (response.type === 'success') {
+      setLoading(true);
       const { idToken } = response.data;
 
       const googleCredential = auth.GoogleAuthProvider.credential(idToken);
@@ -28,29 +34,37 @@ const Login = ({ setActualState, setErrorMessage }) => {
       );
       const firebaseUser = await userCredential.user.getIdToken();
 
-      // --- Insert the spinner here ---
-
       const serverResponse = await sendTokenRequest(firebaseUser);
       console.log(serverResponse);
       if (!serverResponse.ok) {
         setErrorMessage(
-          'Opps... This email doesnt exist in Kaotika world. Try again Bastard!',
+          "Oops...This email doesn't exist in Kaotika world. Try again Bastard!",
         );
         setActualState(AppState.ERROR);
       } else {
         const data = await serverResponse.json();
         console.log(data);
+        setUser(data);
+        setActualState(AppState.HOME);
+        setLoading(false);
       }
     }
-    return (
-      <Container>
-        <Image source={require('../assets/login-screen.jpg')} />
+  };
+  return (
+    <Container>
+      <Image source={require('../assets/login-screen.jpg')} />
+
+      {loading ? (
+        <SpinnerWrapper>
+          <CircleFade size={55} color="#fff" />
+        </SpinnerWrapper>
+      ) : (
         <Button onPress={SignIn}>
           <Text>Sign In with Google</Text>
         </Button>
-      </Container>
-    );
-  };
+      )}
+    </Container>
+  );
 };
 
 export default Login;
