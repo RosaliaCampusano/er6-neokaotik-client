6de8bb2dcd24da9bd52c67e5c1a26f568deed928
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-10-01 13:58:00 +0200

    feat: implement the flow from the screen

--- App.tsx
+++ App.tsx
@@ -1,67 +1,53 @@
 import Login from './components/login';
 import Splash from './components/splash';
-import React, { useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-
-
-import Home from './screens/Home';
-import Settings from './screens/Settings';
-import Stats from './screens/Stats';
-import Lab from './screens/Lab';
-import Error from './screens/Error';
-import { ScrollView } from 'react-native';
-
-
-
-const Tab = createBottomTabNavigator();
-
-//Poner splash screen value
-const SIGN_IN_TO_APP = 0;
-const PLAYER_LOGGED = 1;
-const ERROR_LOGGIN = 2;
+import React, { useState, useEffect } from 'react';
+import { AppState } from './helpers/constants';
+import { GoogleAuth } from 'react-native-google-auth';
+import { webClientId, androidClientId } from './credentials';
+import ErrorMessage from './screens/Error';
 
 function App() {
-  const [actualState, setActualState] = useState(1);
+  useEffect(() => {
+    configuereGoogleAuth();
+  }, []);
 
-
+  const [actualState, setActualState] = useState(AppState.LOGIN);
+  const [errorMessage, setErrorMessage] = useState('');
   return (
     <>
-      {
-        (actualState === SIGN_IN_TO_APP) ?
-          <Login />
-          : (actualState === PLAYER_LOGGED) ?
-            <NavigationContainer>
-              <Tab.Navigator
-                screenOptions={({ route }) => ({
-                  tabBarIcon: ({ focused, color, size }) => {
-                    let iconName;
-
-                    if (route.name === 'Home') {
-                      iconName = 'home';
-                    } else if (route.name === 'Settings') {
-                      iconName = 'settings'
-                    } else if (route.name === 'Stats') {
-                      iconName = 'stats-chart';
-                    } else if (route.name === 'Lab') {
-                      iconName = 'flask'
-                    }
-                    return <Ionicons name={iconName} color={color} size={20} />
-                  },
-
-
-                })}>
-                <Tab.Screen options={{ headerShown: false }} name="Home" component={Home} />
-                <Tab.Screen options={{ headerShown: false }} name="Settings" component={Settings} />
-                <Tab.Screen options={{ headerShown: false }} name="Stats" component={Stats} />
-                <Tab.Screen options={{ headerShown: false }} name="Lab" component={Lab} />
-              </Tab.Navigator>
-            </NavigationContainer>
-            : false
-      }
+      {changeScreen(actualState, setActualState, errorMessage, setErrorMessage)}
     </>
   );
 }
+
+const changeScreen = (
+  actualState: number,
+  setActualState: any,
+  errorMessage: string,
+  setErrorMessage: any,
+) => {
+  switch (actualState) {
+    case AppState.SPLASH_SCREEN:
+      return <Splash setActualState={setActualState} />;
+    case AppState.LOGIN:
+      return (
+        <Login
+          setActualState={setActualState}
+          setErrorMessage={setErrorMessage}
+        />
+      );
+    case AppState.ERROR:
+      return (
+        <ErrorMessage message={errorMessage} setMessage={setErrorMessage} />
+      );
+  }
+};
+
+const configuereGoogleAuth = async () => {
+  await GoogleAuth.configure({
+    webClientId: webClientId,
+    androidClientId: androidClientId,
+  });
+};
 
 export default App;
