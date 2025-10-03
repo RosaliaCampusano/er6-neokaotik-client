b62926b0556075a9d9b6fab7d39f9d92b47bf4c5
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-10-03 08:36:47 +0200

    refactor: seet the api request to render

--- request/tokenResquest.ts
+++ request/tokenResquest.ts
@@ -1,12 +1,15 @@
 export const sendTokenRequest = async (firebaseToken: string) => {
   console.log('Sending Token request to server...');
-  const response = await fetch('http://10.50.0.50:6000/api/', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
+  const response = await fetch(
+    'https://er6-neokaotik-server.onrender.com/api/',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ tokenId: firebaseToken }),
     },
-    body: JSON.stringify({ tokenId: firebaseToken }),
-  });
+  );
 
   console.log(`Token request completed.`);
 
