7f84877acdf1f8a92809cf75e414238d4e66f3e1
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-10-03 08:31:46 +0200

    refactor: change the api for the kaotika server

--- request/tokenResquest.ts
+++ request/tokenResquest.ts
@@ -1,18 +1,14 @@
 export const sendTokenRequest = async (firebaseToken: string) => {
   console.log('Sending Token request to server...');
-  const response = await fetch(
-    // 'http://10.50.0.50:6000/api/',
-    'https://er6-neokaotik-server.onrender.com/api/',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ tokenId: firebaseToken }),
+  const response = await fetch('http://10.50.0.50:6000/api/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
     },
-  );
+    body: JSON.stringify({ tokenId: firebaseToken }),
+  });
 
-  console.log(`Token request completed.`)
+  console.log(`Token request completed.`);
 
   return response;
 };
