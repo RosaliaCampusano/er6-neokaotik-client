4627742696693fb5405b9e7f619149a7260cbb9a
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-10-07 13:54:43 +0200

    refactor(NK-44): modify to have the handle function to sign out of the user and return to the login screen

--- components/singOut.tsx
+++ components/singOut.tsx
@@ -1,7 +1,4 @@
-import React from 'react';
 import { AppState } from '../helpers/constants';
-import { AppProviderContext } from '../helpers/AppProvider';
-import { Text, Button } from '../styled-components/singOutStyle';
 import { GoogleAuth } from 'react-native-google-auth';
 
 const signOut = async () => {
@@ -13,18 +10,8 @@ const signOut = async () => {
   }
 };
 
-const SingOut = () => {
-  const { setActualState, setUser } = AppProviderContext();
-  const handlerPress = () => {
-    signOut();
-    setActualState(AppState.LOGIN);
-    setUser(null);
-  };
-  return (
-    <Button onPress={handlerPress}>
-      <Text>Log out</Text>
-    </Button>
-  );
+export const handlerPress = (setActualState: any, setUser: any) => {
+  signOut();
+  setActualState(AppState.LOGIN);
+  setUser(null);
 };
-
-export default SingOut;
