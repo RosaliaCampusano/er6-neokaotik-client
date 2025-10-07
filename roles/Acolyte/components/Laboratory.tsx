2ae8f7dde438d39a5b380280a4d64e3ad8549783
Rosalia Campusano <rosalia.campusano@ikasle.aeg.eus>
2025-10-07 20:26:50 +0200

    feat(NK-50): implement the logic to hide the bar navigation when the user is inside the lab

--- roles/Acolyte/components/Laboratory.tsx
+++ roles/Acolyte/components/Laboratory.tsx
@@ -1,13 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import QRCode from 'react-native-qrcode-svg';
+import { useNavigation } from '@react-navigation/native';
 import { ImageContainer } from '../css/screenBackground';
 import { AppProviderContext } from '../../../helpers/AppProvider';
 import { LabImage, LabDoor, Button, Text } from '../css/laboratory';
 
 const Lab = () => {
   const { user } = AppProviderContext();
+  const navigation = useNavigation();
   const isInside = false; // --- Change for the isInside user attribute ---
   const [showQR, setShowQR] = useState(false);
+
+  useEffect(() => {
+    navigation.setOptions({
+      tabBarStyle: isInside
+        ? { display: 'none' }
+        : {
+            backgroundColor: '#0a171e',
+            borderColor: '#333',
+          },
+    });
+  }, [navigation, isInside]);
 
   return (
     <>
