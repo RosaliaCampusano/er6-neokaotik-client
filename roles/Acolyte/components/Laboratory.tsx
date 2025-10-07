8e1cd3bca5e6d0d342b6e0135fdf60f3a4cd5064
Rosalia Campusano <rosalia.campusano@ikasle.aeg.eus>
2025-10-07 20:25:18 +0200

    feat(NK-48): implement the logic to generate a QR based on the user email

--- roles/Acolyte/components/Laboratory.tsx
+++ roles/Acolyte/components/Laboratory.tsx
@@ -1,10 +1,44 @@
-import { Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import QRCode from 'react-native-qrcode-svg';
+import { ImageContainer } from '../css/screenBackground';
+import { AppProviderContext } from '../../../helpers/AppProvider';
+import { LabImage, LabDoor, Button, Text } from '../css/laboratory';
+
 const Lab = () => {
+  const { user } = AppProviderContext();
+  const isInside = false; // --- Change for the isInside user attribute ---
+  const [showQR, setShowQR] = useState(false);
+
   return (
     <>
-      <Text>Laboratory</Text>
+      {isInside ? (
+        <ImageContainer
+          source={LabImage}
+          resizeMode="cover"
+          style={{ filter: 'brightness(2)' }}
+        >
+          <Button onPress={() => setShowQR(!showQR)}>
+            <Text>{showQR ? 'Hide QR' : 'Show QR'}</Text>
+          </Button>
+
+          {showQR && QRToScan(user.data.email)}
+        </ImageContainer>
+      ) : (
+        <ImageContainer source={LabDoor} resizeMode="cover">
+          {QRToScan(user.data.email)}
+        </ImageContainer>
+      )}
     </>
   );
 };
+
+const QRToScan = (email: string) => (
+  <QRCode
+    value={email}
+    size={250}
+    color="rgba(87, 175, 216)"
+    backgroundColor="transparent"
+  />
+);
 
 export default Lab;
--- roles/Acolyte/components/Settings.tsx
+++ roles/Acolyte/components/Settings.tsx
@@ -1,10 +1,19 @@
 import { ImageContainer, urlImage } from '../css/screenBackground';
-import SingOut from '../../../components/singOut';
+import { Button, Text } from '../css/laboratory';
+import { handlerPress } from '../../../components/singOut';
+import { AppProviderContext } from '../../../helpers/AppProvider';
 
 const Settings = () => {
+  const { setActualState, setUser } = AppProviderContext();
   return (
     <ImageContainer source={urlImage} resizeMode="cover">
-      <SingOut />
+      <Button
+        onPress={() => {
+          handlerPress(setActualState, setUser);
+        }}
+      >
+        <Text>Log out</Text>
+      </Button>
     </ImageContainer>
   );
 };
