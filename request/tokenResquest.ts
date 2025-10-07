badf7fa51ccd4cbcc51678e371495f9451b68779
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-10-07 12:22:26 +0200

    refactor(NK-62): change the string url to the url constant

--- request/tokenResquest.ts
+++ request/tokenResquest.ts
@@ -1,15 +1,14 @@
+import { URL } from '../helpers/constants';
+
 export const sendTokenRequest = async (firebaseToken: string) => {
   console.log('Sending Token request to server...');
-  const response = await fetch(
-    'https://er6-neokaotik-server.onrender.com/api/',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ tokenId: firebaseToken }),
+  const response = await fetch(URL.render, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
     },
-  );
+    body: JSON.stringify({ tokenId: firebaseToken }),
+  });
 
   console.log(`Token request completed.`);
 
