4aaa876412bb2a8f0b6bb92533cdfe8bb7ce2ad3
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-10-08 10:43:39 +0200

    feat(NK-45): implement the logic to display the scan QR and to return the home screen when a QR is scanner

--- roles/Istvan/components/Home.tsx
+++ roles/Istvan/components/Home.tsx
@@ -1,21 +1,34 @@
-import { ImageContainer, urlImage, Button, Text } from '../css/homeStyle';
+import React from 'react';
+import QRScanner from './Scanner';
 import { handlerPress } from '../../../components/singOut';
 import { AppProviderContext } from '../../../helpers/AppProvider';
+import { ImageContainer, urlImage, Button, Text } from '../css/homeStyle';
+
 const Home = () => {
-  const { setActualState, setUser } = AppProviderContext();
+  const { scan, setScanning, setActualState, setUser } = AppProviderContext();
+
+  const handlerScan = () => {
+    setScanning(!scan);
+  };
   return (
-    <ImageContainer source={urlImage} resizeMode="cover">
-      <Button>
-        <Text>Scan QR</Text>
-      </Button>
-      <Button
-        onPress={() => {
-          handlerPress(setActualState, setUser);
-        }}
-      >
-        <Text>Log out</Text>
-      </Button>
-    </ImageContainer>
+    <>
+      {scan ? (
+        <QRScanner />
+      ) : (
+        <ImageContainer source={urlImage} resizeMode="cover">
+          <Button onPress={handlerScan}>
+            <Text>Scan QR</Text>
+          </Button>
+          <Button
+            onPress={() => {
+              handlerPress(setActualState, setUser);
+            }}
+          >
+            <Text>Log out</Text>
+          </Button>
+        </ImageContainer>
+      )}
+    </>
   );
 };
 
