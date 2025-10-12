f1ae3a0e16b6bb39461a08ea2715fb641772517b
Rosalia Campusano <rosalia.campusano@ikasle.aeg.eus>
2025-10-12 22:19:34 +0200

    feat(NK-73): implement the logic to return the screen when a button is pressed

--- roles/Istvan/IstvanNavigation.tsx
+++ roles/Istvan/IstvanNavigation.tsx
@@ -1,11 +1,11 @@
 import Home from './components/Home';
+import QRScanner from './components/Scanner';
+import { AppProviderContext } from '../../helpers/AppProvider';
+import { IstvanState } from '../../helpers/constants';
 
 const IstvanNavigation = () => {
-  return (
-    <>
-      <Home />
-    </>
-  );
+  const { istvanState } = AppProviderContext();
+  return <>{istvanState === IstvanState.HOME ? <Home /> : <QRScanner />}</>;
 };
 
 export default IstvanNavigation;
