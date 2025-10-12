2caf81743877f56d5d740bb46bcb3bcb5fe70e93
Rosalia Campusano <rosalia.campusano@ikasle.aeg.eus>
2025-10-12 22:21:25 +0200

    feat(NK-73): implment the button logic to return home when the button is pressed

--- roles/Istvan/components/Home.tsx
+++ roles/Istvan/components/Home.tsx
@@ -1,33 +1,31 @@
 import React from 'react';
-import QRScanner from './Scanner';
+import { IstvanState } from '../../../helpers/constants';
 import { handlerPress } from '../../../components/singOut';
 import { AppProviderContext } from '../../../helpers/AppProvider';
 import { ImageContainer, urlImage, Button, Text } from '../css/homeStyle';
 
 const Home = () => {
-  const { scan, setScanning, setActualState, setUser } = AppProviderContext();
+  const { scan, setScanning, setIstvanState, setActualState, setUser } =
+    AppProviderContext();
 
   const handlerScan = () => {
+    setIstvanState(IstvanState.SCANNER);
     setScanning(!scan);
   };
   return (
     <>
-      {scan ? (
-        <QRScanner />
-      ) : (
-        <ImageContainer source={urlImage} resizeMode="cover">
-          <Button onPress={handlerScan}>
-            <Text>Scan QR</Text>
-          </Button>
-          <Button
-            onPress={() => {
-              handlerPress(setActualState, setUser);
-            }}
-          >
-            <Text>Log out</Text>
-          </Button>
-        </ImageContainer>
-      )}
+      <ImageContainer source={urlImage} resizeMode="cover">
+        <Button onPress={handlerScan}>
+          <Text>Scan QR</Text>
+        </Button>
+        <Button
+          onPress={() => {
+            handlerPress(setActualState, setUser);
+          }}
+        >
+          <Text>Log out</Text>
+        </Button>
+      </ImageContainer>
     </>
   );
 };
--- roles/Istvan/components/Scanner.tsx
+++ roles/Istvan/components/Scanner.tsx
@@ -1,16 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
 import {
   useCameraDevice,
   useCodeScanner,
   Camera,
+  CameraPermissionStatus,
 } from 'react-native-vision-camera';
+import { StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { IstvanState } from '../../../helpers/constants';
 import { AppProviderContext } from '../../../helpers/AppProvider';
+import { Container, Button, Text, Overlay } from '../css/scannerStyle';
 
 const QRScanner = () => {
-  const device = useCameraDevice('back');
-  const [hasPermission, setHasPermission] = useState(false);
-  const { scan, setScanning } = AppProviderContext();
+  const device: any = useCameraDevice('back');
+  const [hasPermission, setHasPermission] =
+    useState<CameraPermissionStatus | null>(null);
+  const { scan, setScanning, setIstvanState } = AppProviderContext();
 
   const codeScanner = useCodeScanner({
     codeTypes: ['qr', 'ean-13', 'upc-a'],
@@ -25,23 +29,35 @@ const QRScanner = () => {
   useEffect(() => {
     (async () => {
       const status = await Camera.requestCameraPermission();
-      setHasPermission(status !== 'denied');
+      setHasPermission(status);
     })();
   }, []);
 
+  useEffect(() => {
+    if (!scan) {
+      setIstvanState(IstvanState.HOME);
+    }
+  }, [scan]);
+
   return (
-    device != null &&
-    hasPermission && (
-      <>
-        <Camera
-          style={StyleSheet.absoluteFillObject}
-          device={device}
-          isActive={scan}
-          codeScanner={codeScanner}
-          enableZoomGesture={true}
-        />
-      </>
-    )
+    <Container>
+      <Camera
+        style={StyleSheet.absoluteFillObject}
+        device={device}
+        isActive={scan}
+        codeScanner={codeScanner}
+        enableZoomGesture
+      />
+      <Overlay>
+        <Button
+          onPress={() => {
+            setScanning(false);
+          }}
+        >
+          <Text>X</Text>
+        </Button>
+      </Overlay>
+    </Container>
   );
 };
 
